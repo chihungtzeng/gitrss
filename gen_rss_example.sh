40ae5c86e0adadb486a8ded77be74bb1e1ae34cc
clark tseng <clark@elsa-group.legal>
2017-11-26 23:02:38 +0800

    Add README.

--- gen_rss_example.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-readonly top=$(readlink -e $(dirname $0))
-repo=~/openSource/chromium.git/src
-export PYTHONPATH=${top}:${top}/gitrss
-python -m gitrss --repo ${repo} --output-file /mnt/ramdisk/rss.xml
